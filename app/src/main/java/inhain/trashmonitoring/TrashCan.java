fbe528f60abe6e15e2b77af67149d64fc8905f4d
Hanul <nul010@naver.com>
2017-03-19 00:45:19 +0900

    리스트뷰 연동

--- app/src/main/java/inhain/trashmonitoring/TrashCan.java
+++ app/src/main/java/inhain/trashmonitoring/TrashCan.java
@@ -1,12 +1,14 @@
 package inhain.trashmonitoring;
 
 public class TrashCan {
+    int idx;
     String address;
     String location;
     int percent;
     String color;
 
-    TrashCan(String address, String location, int percent, String color) {
+    TrashCan(int idx, String address, String location, int percent, String color) {
+        this.idx=idx;
         this.address = address;
         this.location = location;
         this.percent = percent;
