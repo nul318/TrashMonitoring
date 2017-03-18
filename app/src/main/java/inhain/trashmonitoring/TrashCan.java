eb116aba7c1ebbf0f1047954f8c5847c8b60a176
samsung\user <hongsik3147@naver.com>
2017-03-19 04:48:47 +0900

    홍식완성본

--- app/src/main/java/inhain/trashmonitoring/TrashCan.java
+++ app/src/main/java/inhain/trashmonitoring/TrashCan.java
@@ -7,18 +7,15 @@ public class TrashCan {
     String address;
     String location;
     int percent;
-
-   String color;
-    static String color2;
+    String color;
     Drawable draw;
 
-    TrashCan(int idx, String address, String location, int percent, String color, String color2, Drawable draw) {
+    TrashCan(int idx, String address, String location, int percent, String color, Drawable draw) {
         this.idx=idx;
         this.address = address;
         this.location = location;
         this.percent = percent;
         this.color = color;
-        this.color2 = color2;
         this.draw = draw;
     }
 }
